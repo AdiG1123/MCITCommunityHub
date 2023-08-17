903ea4f8ecd8c0645b950b6ba65785df8e360fac
Aditya Garg <gargkaditya@gmail.com>
2023-08-17 12:43:32 -0700

    add sub field to the new user creation query

--- server/controllers/users-cntrl.js
+++ server/controllers/users-cntrl.js
@@ -2,8 +2,8 @@ const pool = require("./db");
 
 // pulls information for a single user
 exports.singleUser = async function user(userid){
-
-  const query = `SELECT * FROM "User" WHERE "userID" = $1`;
+  const query = `SELECT * FROM "User" 
+  WHERE "User"."userID" = $1`;
 
   try {
       const result = await pool.query(query, [userid]);
@@ -16,13 +16,13 @@ exports.singleUser = async function user(userid){
 }
 
 // creates a single user
-exports.createUser = async function createUser(body){
+exports.createUser = async function createUser(body, sub){
 
   const query = `INSERT INTO "User" ("name", "anonName", "timeZone", "expectedGraduation", 
   "industry", "fulltimeStudentStatus", "inTurtleClub", "mcitEmailNotifications",
   "mcitConnectEnable", "mcitConnectEmailNotifications", "email", "linkedinURL", 
-  "preferredContactMethod", "bio", "marketOutcome", "workStatus", "startSemester") 
-  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17) 
+  "preferredContactMethod", "bio", "marketOutcome", "workStatus", "startSemester", "sub") 
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18) 
   RETURNING "userID"`
   
   try {
@@ -32,7 +32,7 @@ exports.createUser = async function createUser(body){
         body.industry, body.fulltimeStudentStatus, body.inTurtleClub, 
         body.mcitEmailNotifications, body.mcitConnectEnable, body.mcitConnectEmailNotifications, 
         body.email, body.linkedinURL, body.preferredContactMethod, body.bio, body.marketOutcome, 
-        body.workStatus, body.startSemester]);
+        body.workStatus, body.startSemester, sub]);
     const newUserID = result.rows.length > 0 ? result.rows : null;
     return newUserID;
 } catch (error){
