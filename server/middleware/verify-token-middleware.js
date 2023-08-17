9e5dfdfa840eda80bf0cab81adce6530eba4c235
Aditya Garg <gargkaditya@gmail.com>
2023-08-17 13:42:03 -0700

    Clean up comments and add comments to clarify code sections

--- server/middleware/verify-token-middleware.js
+++ server/middleware/verify-token-middleware.js
@@ -1,5 +1,5 @@
 const { OAuth2Client } = require('google-auth-library');
-const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID); // Specify your Google Client ID here
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 const verifyToken = async (req, res, next) => {
   const authHeader = req.headers.authorization;
@@ -12,9 +12,10 @@ const verifyToken = async (req, res, next) => {
   }
 
   try {
+    // verifies the 'aud', 'exp', and 'iss' claims
     const ticket = await client.verifyIdToken({
       idToken: idToken,
-      audience: process.env.GOOGLE_CLIENT_ID, // Specify your Google Client ID here
+      audience: process.env.GOOGLE_CLIENT_ID,
     });
     const payload = ticket.getPayload();
     
@@ -23,7 +24,9 @@ const verifyToken = async (req, res, next) => {
     if (domain != "seas.upenn.edu"){
       return req.status(403).json({message: 'Invalid email domain'})
     }
+    // add the payload as req.user
     req.user = payload;
+    // move to next route handler if there are no errors
     next();
   } catch (error) {
     console.error('Error verifying ID token:', error);
