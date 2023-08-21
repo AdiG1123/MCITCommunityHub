7102e0ad78d4dc72c458acb7c4d0db2248fe611e
Aditya Garg <gargkaditya@gmail.com>
2023-08-20 20:10:35 -0700

    separate course taken and current course checks

--- server/controllers/users-cntrl.js
+++ server/controllers/users-cntrl.js
@@ -2,35 +2,37 @@ const pool = require("./db");
 
 // pulls information for a single user
 exports.singleUserByUserID = async function user(userid){
-  const query = `SELECT "User".*, coursenum.courseTaken, coursetaking.courseTaking
+  // const query = `SELECT "User".*, coursenum.courseTaken, coursetaking.courseTaking
+  // FROM "User"
+  
+  // INNER JOIN (
+  //   SELECT array_agg(course.coursenumber) as courseTaken, taken."userID"
+  //   FROM "Courses" AS course
+
+  //   INNER JOIN "CoursesTaken" as taken
+  //   ON taken."courseID" = course."courseID"
+
+  //   WHERE taken."userID" = $1
+  //   GROUP BY taken."userID"
+  // ) AS coursenum
+  // ON coursenum."userID" = "User"."userID"
+
+  // INNER JOIN (
+  //   SELECT array_agg(course.coursenumber) as courseTaking, taking."userID"
+  //   FROM "Courses" AS course
+
+  //   INNER JOIN "CurrentCourses" as taking
+  //   ON taking."courseID" = course."courseID"
+
+  //   WHERE taking."userID" = $1
+  //   GROUP BY taking."userID"
+  // ) AS coursetaking
+  // ON coursetaking."userID" = "User"."userID"
+  
+  // WHERE "User"."userID" = $1`;
+  const query = `SELECT *
   FROM "User"
-  
-  INNER JOIN (
-    SELECT array_agg(course.coursenumber) as courseTaken, taken."userID"
-    FROM "Courses" AS course
-
-    INNER JOIN "CoursesTaken" as taken
-    ON taken."courseID" = course."courseID"
-
-    WHERE taken."userID" = $1
-    GROUP BY taken."userID"
-  ) AS coursenum
-  ON coursenum."userID" = "User"."userID"
-
-  INNER JOIN (
-    SELECT array_agg(course.coursenumber) as courseTaking, taking."userID"
-    FROM "Courses" AS course
-
-    INNER JOIN "CurrentCourses" as taking
-    ON taking."courseID" = course."courseID"
-
-    WHERE taking."userID" = $1
-    GROUP BY taking."userID"
-  ) AS coursetaking
-  ON coursetaking."userID" = "User"."userID"
-  
-  WHERE "User"."userID" = $1`;
-
+  WHERE "User"."userID" = $1`
   try {
       const result = await pool.query(query, [userid]);
       const userInfo = result.rows.length > 0 ? result.rows[0] : null;
@@ -40,6 +42,52 @@ exports.singleUserByUserID = async function user(userid){
   }
 
 }
+
+exports.checkCoursesTaken = async function courseTaken(userid){
+  
+  const query = `SELECT taken."userID", array_agg(taken."courseID") AS "coursesTakenID", array_agg(course.coursenumber) AS "coursesTaken"
+  FROM (SELECT * 
+    FROM "CoursesTaken" as intaken 
+    WHERE intaken."userID" = $1) AS taken
+  
+  INNER JOIN (SELECT "Courses"."courseID", "Courses"."coursenumber" FROM "Courses") as course
+  ON course."courseID" = taken."courseID"
+    
+  
+  GROUP BY taken."userID";`
+  try {
+      const result = await pool.query(query, [userid]);
+      const userInfo = result.rows.length > 0 ? result.rows[0] : null;
+      return userInfo;
+  } catch (error){
+      throw error
+  }
+
+}
+
+exports.checkCurrentCourses = async function currentCourse(userid){
+  
+  const query = `SELECT taking."userID", array_agg(taking."courseID") AS "coursesTakingID", array_agg(course.coursenumber) AS "coursesTaking"
+  FROM (SELECT * 
+    FROM "CurrentCourses" AS intaken 
+    WHERE intaken."userID" = $1) AS taking
+  
+  INNER JOIN (SELECT "Courses"."courseID", "Courses"."coursenumber" FROM "Courses") as course
+  ON course."courseID" = taking."courseID"
+    
+  WHERE taking."userID" = $1
+  GROUP BY taking."userID";`
+  try {
+      const result = await pool.query(query, [userid]);
+      const userInfo = result.rows.length > 0 ? result.rows[0] : null;
+      return userInfo;
+  } catch (error){
+      throw error
+  }
+
+}
+
+
 
 exports.singleUserBySub = async function user(sub){
   const query = `SELECT * FROM "User" 
--- server/routes/users-routes.js
+++ server/routes/users-routes.js
@@ -41,9 +41,30 @@ router.get('/:userID', async (req, res) => {
     const userID = req.params.userID;
     try {
       const userinfo = await users.singleUserByUserID(userID);
-  
+      
       if (userinfo) {
+        
+        const coursesTaken = await users.checkCoursesTaken(userID);
+        console.log(coursesTaken);
+        const coursesTaking = await users.checkCurrentCourses(userID);
+        console.log(coursesTaking);
+        if (coursesTaken != null){
+          userinfo["coursesTaken"] = coursesTaken["coursesTaken"];
+          userinfo["coursesTakenID"] = coursesTaken["coursesTakenID"];
+        }else{
+          userinfo["coursesTaken"] = [];
+          userinfo["coursesTakenID"] = [];
+        }
+        if (coursesTaking != null){
+          userinfo["coursesTaking"] = coursesTaking["coursesTaking"];
+          userinfo["coursesTakingID"] = coursesTaking["coursesTakingID"];
+        }else{
+          userinfo["coursesTaking"] = [];
+          userinfo["coursesTakingID"] = [];
+        }
+        console.log(userinfo);
         res.json(userinfo); // Respond with course data if found
+        
       } else {
         res.status(404).json({ message: 'User not found' }); // Respond with a 404 status code if user not found
       }
