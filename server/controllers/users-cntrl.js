76b4272f01a7c37b24cac53f1a5e2505dd296971
Aditya Garg <gargkaditya@gmail.com>
2023-08-21 17:27:51 -0700

    Clean up sungleUserbyUserID function

--- server/controllers/users-cntrl.js
+++ server/controllers/users-cntrl.js
@@ -2,37 +2,11 @@ const pool = require("./db");
 
 // pulls information for a single user
 exports.singleUserByUserID = async function user(userid){
-  // const query = `SELECT "User".*, coursenum.courseTaken, coursetaking.courseTaking
-  // FROM "User"
   
-  // INNER JOIN (
-  //   SELECT array_agg(course.coursenumber) as courseTaken, taken."userID"
-  //   FROM "Courses" AS course
-
-  //   INNER JOIN "CoursesTaken" as taken
-  //   ON taken."courseID" = course."courseID"
-
-  //   WHERE taken."userID" = $1
-  //   GROUP BY taken."userID"
-  // ) AS coursenum
-  // ON coursenum."userID" = "User"."userID"
-
-  // INNER JOIN (
-  //   SELECT array_agg(course.coursenumber) as courseTaking, taking."userID"
-  //   FROM "Courses" AS course
-
-  //   INNER JOIN "CurrentCourses" as taking
-  //   ON taking."courseID" = course."courseID"
-
-  //   WHERE taking."userID" = $1
-  //   GROUP BY taking."userID"
-  // ) AS coursetaking
-  // ON coursetaking."userID" = "User"."userID"
-  
-  // WHERE "User"."userID" = $1`;
   const query = `SELECT *
   FROM "User"
   WHERE "User"."userID" = $1`
+  
   try {
       const result = await pool.query(query, [userid]);
       const userInfo = result.rows.length > 0 ? result.rows[0] : null;
